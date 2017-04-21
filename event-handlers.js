6e9b97d6ac87a491c6aa7de46e056f521472092f
Hanna Sababa <hzsababa@outlook.com>
2017-04-21 15:31:36 +0300

    Added code for while and for loops

--- event-handlers.js
+++ event-handlers.js
@@ -6,3 +6,19 @@ $(document).ready(function() {
   	$('img').fadeIn(1000);
   });
 });
+
+$(document).ready(() => {
+	$(document).on('click', "img", (e) => {
+		/*
+    let number = 3;
+    while(number > 0) {
+    	alert(number);
+      //number = number - 1;
+      --number;
+    }
+    */
+    
+    for(let i = 3; i > 0; --i)
+    	alert(i);
+  });
+});
